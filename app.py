4cae77e8723b5fe1797aedaaa31a9ceab9e9ce04
LIvE Tech <samangoonerathne@gmail.com>
2023-04-07 09:44:53 +0530

    update sctipt

--- app.py
+++ app.py
@@ -3,6 +3,13 @@ from flask import Flask,request,send_from_directory
 
 app = Flask(__name__)
 
+@app.route('/files/', methods = ['GET'])
+def list_files():
+    retval = ''
+    for item in os.scandir(os.getcwd()):
+        if item.is_file():
+            retval += '<a href="/file/{}"></a><br>'.format(item.name)
+    return retval
 @app.route('/file/<filename>', methods = ['GET'])
 def getfile(filename):
     return send_from_directory(os.getcwd(), filename)
